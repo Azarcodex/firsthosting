f3d5d35cda9cd63f0b6d38214e13607a20a67014
Mohammed Azarin vp <azarcoder@gmail.com>
2026-01-08 11:20:28 +0530

    fix new name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello world how are you,I am Azarin" });
+  res.status(200).json({ message: "Hello world how are you,I am Akshay and ayoob" });
 });
 
 app.listen(8000, () => {
